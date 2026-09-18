4e4567590d079194b619c36393c432b3e9972ec1
saikartheekkotni623-eng <saikartheekkotni623@gmail.com>
2026-09-18 19:28:16 +0530

    Add files via upload

---
+++ and_or_not.py
@@ -0,0 +1,36 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#bitwise operators
+#~,|,^,&,<<,>>
+a=4
+b=6
+bin(4)
+'0b100'
+bin(6)
+'0b110'
+a&b
+4
+a=2
+b=4
+bin(2)
+'0b10'
+bin(4)
+'0b100'
+a&b
+0
+a=5
+b=7
+>>> a|b
+7
+>>> a=4
+>>> b=8
+>>> a|b
+12
+>>> #not
+>>> a=9
+>>> ~a
+-10
+>>> #formula is -(a+1)
+>>> a=5
+>>> ~a
+-6
