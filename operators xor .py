6e666e3ebfac7661348a91ee8ea665b0c72c1a23
saikartheekkotni623-eng <saikartheekkotni623@gmail.com>
2026-09-19 19:45:17 +0530

    Add files via upload

---
+++ operators xor .py
@@ -0,0 +1,50 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#bitwise xor
+a=3
+b=6
+bin(3)
+'0b11'
+bin(6)
+'0b110'
+a=4
+b=7
+bin(4)
+'0b100'
+a^b
+3
+a=12
+b=23
+bin(12)
+'0b1100'
+bin(23)
+'0b10111'
+a^b
+27
+#<<
+a=3
+a<<2
+12
+bin(3)
+'0b11'
+a=5
+bin(5)
+'0b101'
+a<<3
+40
+#>>
+a=4
+>>> a>>2
+1
+>>> bin(4)
+'0b100'
+>>> a=6
+>>> a>>3
+0
+>>> a=7
+>>> a>>3
+0
+>>> #<<
+>>> a=2
+>>> a<<3
+16
