a961c975f3e41aadfcf2c5bc902d94e5f36512bc
saikartheekkotni623-eng <saikartheekkotni623@gmail.com>
2026-09-21 19:42:54 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,91 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#arthimatic
+a=5
+b=9
+print(a+b)
+14
+print(a-b)
+-4
+print(a*b)
+45
+print(a//b)
+0
+print(a/b)
+0.5555555555555556
+
+print(a**b)
+1953125
+print(a%b)
+5
+#assignment
+a=4
+b=5
+a+=9
+a
+13
+a-=3
+a
+10
+a*=3
+a
+30
+a/=8
+a
+3.75
+a//=1
+a
+3.0
+#comparison
+a=34
+b=98
+a>b
+False
+a<b
+True
+a<=b
+True
+a>=b
+False
+a!=b
+True
+a==b
+False
+a=9
+b=9.0
+a==b
+True
+#logical
+a=9
+b=17
+a<b and b>a
+True
+a=<b and b>=a
+SyntaxError: invalid syntax
+a<=b and b=>a
+SyntaxError: cannot assign to expression
+a<=b and b>=a
+True
+a>b or b>a
+True
+a<=b or b<=a
+True
+a!=b or a==b
+True
+not True
+False
+not False
+True
+>>> #identity
+>>> #identity
+>>> a=9
+>>> type(a) is int
+True
+>>> type(a) is not int
+False
+>>> KeyboardInterrupt
+>>> type(a) is float
+False
+>>> type(a) is not int
+False
+>>> #membership
