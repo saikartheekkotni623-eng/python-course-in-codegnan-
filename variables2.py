275121dba7bbd31f255155594ee19969c6e9f6c2
saikartheekkotni623-eng <saikartheekkotni623@gmail.com>
2026-09-21 22:31:50 +0530

    Add files via upload

---
+++ variables2.py
@@ -0,0 +1,97 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+a=10
+print(a)
+10
+print(3)
+3
+print(b)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(b)
+NameError: name 'b' is not defined
+b=5
+print(b)
+5
+c=40
+print(c)
+40
+d=9
+print(d)
+9
+4=40
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a4=90
+print(a4)
+90
+d90
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    d90
+NameError: name 'd90' is not defined
+d5=90
+print(d5)
+90
+a0123456789=98
+print(a0123456789)
+98
+@=0
+SyntaxError: invalid syntax
+$=6
+SyntaxError: invalid syntax
+_=4
+print(_)
+4
+_5=7
+9_=3
+SyntaxError: invalid decimal literal
+if=4
+SyntaxError: invalid syntax
+else=6
+SyntaxError: invalid syntax
+first name="sai"
+SyntaxError: invalid syntax
+first_name="sai"
+firstname="sai"
+a=5,b=7
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=5;b=7
+print(a+b)
+12
+a,b=6,8
+print(a+b)
+14
+a=6
+b=9
+print(a+b)
+15
+a=4,5,6,7,8,9
+print(a)
+(4, 5, 6, 7, 8, 9)
+a,b,c=4,5,6
+print(a,b,c)
+4 5 6
+a,b,c=5,6,7,8,9,10
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    a,b,c=5,6,7,8,9,10
+ValueError: too many values to unpack (expected 3, got 6)
+name="karthik"
+print(name)
+karthik
+city="vizag"
+print(city)
+vizag
+>>> a,b,c=(6,7,8)
+>>> print(a,b,c)
+6 7 8
+>>> a=6
+>>> print(a)
+6
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#52>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a4'?
