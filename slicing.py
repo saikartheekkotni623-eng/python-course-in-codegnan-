f90ce0f3839a08de43dd26cd24899565311c023c
saikartheekkotni623-eng <saikartheekkotni623@gmail.com>
2026-09-21 19:49:29 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,12 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #slicing
+>>> a="codegnan"
+>>> a[0:3]
+'cod'
+>>> a[:4]
+'code'
+>>> a[4:]
+'gnan'
+>>> a[4:8]
+'gnan'
