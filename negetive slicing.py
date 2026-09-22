850bd6d020cac827acadc8c9c8732dfda28a1faa
saikartheekkotni623-eng <saikartheekkotni623@gmail.com>
2026-09-22 21:02:57 +0530

    Add files via upload

---
+++ negetive slicing.py
@@ -0,0 +1,19 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#negetive slicing
+a="codegnan it solutions"
+a[-17:-13]
+'gnan'
+>>> a[-12:-10]
+'it'
+>>> a[-9:-1]
+'solution'
+>>> a[-9:]
+'solutions'
+>>> b="vijayawada is royal city"
+>>> b[-4:]
+'city'
+>>> b[-10:-5]
+'royal'
+>>> b[-26:-16]
+'vijayawa'
